d0e34be0e9c98ffd2ff747ff6aa088a1855d8467
CICE-T647 <cicet647@gmail.com>
2019-11-25 21:19:37 +0100

    index

--- index.js
+++ index.js
@@ -4,20 +4,11 @@ const boolean = true;
 
 if (number === 5) {
   var number = 10;
-  console.log("number dentro del condicional", number);
 
   let string = "5";
-  console.log("String dentro del condicional", string);
 
   const boolean = false;
-
-  console.log(boolean); // false
 }
 
 string = "un numero cualquiera";
 number = 10000;
-
-console.log("boolean", boolean); // true
-
-console.log("number fuera del condicional", number); // 10
-console.log("String fuera del condicional", string); // 1
